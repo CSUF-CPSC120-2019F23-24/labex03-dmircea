1d03ea53c962a72eefc504d6fffee9b76ae9a9ff
Mircea Dumitrache <mircea@Mirceas-MacBook-Pro.local>
2019-09-07 15:29:26 -0700

    Finished Project!

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -4,11 +4,11 @@
 
 int main()
 {
-  // TODO#1: try changing these values to others found in the ASCII character set
+  // DONE#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 43;   // decimal
+  char my_char2 = 0x2B; // hex
+  char my_char3 = '+';  // character
 
   std::cout << "Char1: " << my_char1 << std::endl;
   std::cout << "Char2: " << my_char2 << std::endl;
@@ -16,30 +16,39 @@ int main()
 
   std::cout << std::endl; // blank line to separate the different exercises
 
-  // TODO#2: the following statement displays a plus sign
+  // DONE#2: the following statement displays a plus sign
   // add 3 more cout statements that all print the same thing - a plus sign
   // however, use a different method to do so for each cout statement
   // hint: use the ASCII character set
-  std::cout << "+" << std::endl;
+  std::cout << "Pure string: "<< "+" << std::endl;
+  std::cout << "Integer casting: " << char(43) << std::endl;
+  std::cout << "Hexadecimal casting: " << char(0x2B) << std::endl;
+  std::cout << "Octal casting: " << char(053) << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
-  // TODO#3: update the following statement to include escape characters
+  // DONE#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << "\'Y\'" << "   \"This sentence is in double quotes!\"" << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
-  // TODO#4: try changing the variable types and the data types in the
+  // DONE#4: try changing the variable types and the data types in the
   // statements below to see what is returned by the sizeof operator
   bool my_bool = 1;
   std::cout << my_bool << ' ' << sizeof(my_bool) << '\n';
   short my_int = 1;
   std::cout << my_int << ' ' << sizeof(my_int) << '\n';
 
-  double amount;
+  double amount = 1;
+  std::cout << "A bool is stored in: " << sizeof(bool) << " bytes\n";
+  std::cout << "A char is stored in: " << sizeof(char) << " bytes\n";
+  std::cout << "A short is stored in: " << sizeof(short) << " bytes\n";
+  std::cout << "An int is stored in: " << sizeof(int) << " bytes\n";
+  std::cout << "A long is stored in: " << sizeof(int) << " bytes\n";
   std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
+  std::cout << "A double is stored in: " << sizeof(double) << " bytes\n";
   std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
 
   return 0;
--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,18 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include<iostream>
+
+int main()
+{
+  int height;
+  int feet, inches;
+
+  std::cout << "How tall is the person in inches: "; std::cin >> height;
+
+  feet = height / 12;
+  inches = height % 12;
+
+  std::cout << "The person is: " << feet << "\'" << inches << "\"" << std::endl;
+
+  return 0;
+}
